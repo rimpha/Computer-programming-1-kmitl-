7fec0333bbf10fd40a52b72e4216a59f4ea1a87e
rimpha <phuex7619@gmail.com>
2026-07-17 04:48:16 +0000

    week(2) lab and exercise

--- lab-2/ex05.c
+++ lab-2/ex05.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 
 int main() {
-    char name[50];
+    char name[50], lastname[50];
     int Age;
     float height;
     char uni_name[50];
 
     printf("Enter your full name: ");
-    scanf(" %[^\n]", name);
+    scanf("%s %s", name, lastname);
 
-    printf("Enter your age : ");
+    printf("Enter your age: ");
     scanf("%d", &Age);
 
-    printf("Enter your height : ");
+    printf("Enter your height: ");
     scanf("%f", &height);
 
-    printf("Enter your University name : ");
+    printf("Enter your University name: ");
     scanf(" %[^\n]", uni_name);
 
-    printf("Hi Everyone. This is %s from %s. I am %d years old and my height is %.1f cm tall\n", name, uni_name, Age, height);
+    printf("\nHi! Everyone. This is %c.%s from %s. I am %d years old\n",
+           lastname[0], name, uni_name, Age);
+    printf("and my height is %.1f cm tall.\n", height);
 
     return 0;
 }
